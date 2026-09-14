9b6f66cd93594bd2249d5b99aa3bc5e38da44c08
kephy58-a11y <kephy58@gmail.com>
2026-09-14 22:04:32 +0300

    Add files via upload

--- lib/screens/attendance_screen.dart
+++ lib/screens/attendance_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:share_plus/share_plus.dart';
-import 'package:cross_file/cross_file.dart';
 import 'dart:convert';
 
 import '../services/attendance_service.dart';
